a0d5ae47230e77e284708772491c750456f6e325
jenbyyny <jbyyny@users.noreply.github.com>
2017-10-19 09:38:40 -0600

    added read more / less jquery

--- js/index.js
+++ js/index.js
@@ -5,10 +5,25 @@ $(document).ready(function(){
 
 	// function doSomething(){
 	// 	event.preventDefault();
-	// }
+	// enter what you want it to do here
+	// } //
 
 
-//Learn more on click push content down and show more text and change tetx to read less
+//Read more on click push content down and show more text and change tetx to show less//
+$('.readmore').click(expandReadMore);
+function expandReadMore(){
+	event.preventDefault();
+	$("#show-this-on-click").slideToggle();
+}
+
+$('.readless hide').click(expandReadLess);
+function expandReadLess(){
+	event.preventDefault();
+	$("#show-this-on-click").slideToggle();
+}
+
+
+//Learn more on click push content down and show more text and change text to show less//
 
 $('.learnmore').click(expandLearn);
 function expandLearn(){
