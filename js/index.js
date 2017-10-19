98317b247065389b3967cedcbe5fc4ae61419052
jenbyyny <jbyyny@users.noreply.github.com>
2017-10-18 21:13:23 -0600

    added js file

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+//this line above always goes at the top//
+
+	$("a")click(doSomething();
+
+	function doSomething(){
+		event.preventDefault();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//this closes the $(document) line at top//
+	});
