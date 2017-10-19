b6578b79d8225f313e3d5b0a0614003e61291649
jenbyyny <jbyyny@users.noreply.github.com>
2017-10-19 09:14:17 -0600

    start of assignment 3 with js, jquery

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
 //this line above always goes at the top//
 
-	$("a")click(doSomething();
+	// $("a")click(doSomething();
 
-	function doSomething(){
-		event.preventDefault();
-	}
+	// function doSomething(){
+	// 	event.preventDefault();
+	// }
 
+
+//Learn more on click push content down and show more text and change tetx to read less
+
+$('.learnmore').click(expandLearn);
+function expandLearn(){
+	event.preventDefault();
+	$("#learnmoretext").slideToggle();
+}
 
 
 
